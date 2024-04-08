9b5046ed3823f184d8dcbab699982ca9786519ca
DarklMoon <phuriwat757@gmail.com>
2024-04-08 20:28:09 +0700

    add: form-input

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,14 +1,60 @@
-import { CardWrapper } from "@/components/auth/card-wrapper"
+"use client";
 
- export const LoginForm = () => {
-     return(
-         <CardWrapper
-            headerLabel="Welcome back"
-            backButtonLabel="Don't have an account?"
-            backButtonHref="/auth/register"
-            showSocial
-            >
-             Login Form!
-         </CardWrapper>
-     )
- }
+import * as z from "zod";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "@/components/ui/input";
+
+import { LoginSchema } from "@/schemas";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+import { CardWrapper } from "@/components/auth/card-wrapper";
+
+export const LoginForm = () => {
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  return (
+    <CardWrapper
+      headerLabel="Welcome back"
+      backButtonLabel="Don't have an account?"
+      backButtonHref="/auth/register"
+      showSocial
+    >
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(() => {})} className="space-y-6">
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel htmlFor="email">Email</FormLabel>
+                  <FormControl>
+                    <Input 
+                    {...field}
+                    placeholder="darkl.moon@example.com"
+                    type="email" />
+                  </FormControl>
+                </FormItem>
+              )}
+            ></FormField>
+          </div>
+        </form>
+      </Form>
+    </CardWrapper>
+  );
+};
