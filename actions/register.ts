c38477a08cc6d7ded87ee3f18c594024c669f8b1
DarklMoon <phuriwat757@gmail.com>
2024-04-16 23:13:12 +0700

    add: create-user (without-verified)

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,10 @@
 "use server";
 
 import * as z from "zod";
+import { db } from "@/lib/db";
+import bcrypt from "bcrypt";
+
+import { getUserByEmail } from '@/data/user';
 import { RegisterSchema } from "@/schemas";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
@@ -10,5 +14,24 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
         return {error: "Invalid fields!"};
     }
 
-    return {success: "Email sent!"};
+    const { email, password, name } = validatedFields.data;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const existingUser = await getUserByEmail(email);
+
+    if(existingUser){
+        return { error: "Email already in use!" }
+    }
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword,
+        }
+    })
+
+    // TODO: Send verification token email
+
+    return {success: "User created!"};
 };
