0a86e4ac2d444498f35004dfa9d1e22c1454dde9
DarklMoon <phuriwat757@gmail.com>
2024-04-27 23:59:11 +0700

    add: routes-file

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,7 +1,11 @@
-const SettingsPage = () => {
+import { auth } from "@/auth";
+
+const SettingsPage = async () => {
+    const session = await auth();
+
     return ( 
         <div>
-            Settings Page
+            {JSON.stringify(session)}
         </div>
      );
 }
