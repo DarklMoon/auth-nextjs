2040a871afab3c03377933c27a366db24d900931
DarklMoon <phuriwat757@gmail.com>
2024-04-28 23:41:27 +0700

    add: authen-00

--- middleware.ts
+++ middleware.ts
@@ -6,9 +6,22 @@ import { DEFAULT_LOGIN_REDIRECT, apiAuthPrefix, authRoutes, publicRoutes } from 
 const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
+  const { nextUrl } = req;
   const isLoggedIn = !!req.auth;
-  console.log("Middleware:", req.nextUrl.pathname)
-  console.log("IsLoggedIn:", isLoggedIn)
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix)
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  if(isApiAuthRoute){
+      return null;
+  }
+  if(isAuthRoute){
+      if(isLoggedIn){
+        return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
+      }
+      return null
+  }
 });
 
 // Optionally, don't invoke Middleware on some paths
