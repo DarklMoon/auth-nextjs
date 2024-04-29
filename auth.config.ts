1bb3b20372f8d8620ece2126ef70f83c1936ec15
DarklMoon <phuriwat757@gmail.com>
2024-04-30 00:08:20 +0700

    add: credential-provider-config

--- auth.config.ts
+++ auth.config.ts
@@ -1,3 +1,4 @@
+import bcrypt from "bcrypt"
 import type { NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 
@@ -13,7 +14,14 @@ export default { providers: [
                 const { email, password } = validatedFields.data;
 
                 const user = await getUserByEmail(email)
-                // if(!user || !userP)
+                if(!user || !user.password) return null;
+
+                const passwordMatch = await bcrypt.compare(
+                    password,
+                    user.password,
+                );
+
+                if(passwordMatch) return user;
             }
             return null
         }
