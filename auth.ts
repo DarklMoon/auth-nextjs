073f46d41b06590c6e67a4fd63376220134721ca
DarklMoon <phuriwat757@gmail.com>
2024-05-03 22:53:24 +0700

    add: callback-00

--- auth.ts
+++ auth.ts
@@ -11,6 +11,18 @@ export const {
     signIn,
     signOut,
     } = NextAuth({    
+    callbacks: {
+        async session({ session, token }) {
+            console.log({sessionToken: token,session})
+            return session;
+        },
+        async jwt({ token }) {
+            // Add access_token to the token right after signin
+            token.customField = "You can customField";
+            console.log({ token });
+            return token;
+        },
+    },
     adapter: PrismaAdapter(db),
     session: {strategy: "jwt"},
     ...authConfig
