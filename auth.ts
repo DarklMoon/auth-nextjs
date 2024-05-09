7c7ced46325f77cbb2cdf3485c5512cf57a59192
DarklMoon <phuriwat757@gmail.com>
2024-05-09 23:02:57 +0700

    add: user-role

--- auth.ts
+++ auth.ts
@@ -2,6 +2,8 @@ import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter"
 // import GitHub from "next-auth/providers/github";
 // import Google from "next-auth/providers/google";
+
+import { getUserById } from "@/data/user";
 import { db } from "@/lib/db";
 import authConfig from "@/auth.config"; 
 
@@ -13,13 +15,17 @@ export const {
     } = NextAuth({    
     callbacks: {
         async session({ session, token }) {
-            // console.log({
-            //     sessionToken: token,
-            //     session
-            // })
+            console.log({
+                sessionToken: token,
+                session
+            })
 
             if(token.sub && session.user){
                 session.user.id = token.sub;
+            }
+
+            if( token.role && session.user){
+                session.user.role = token.role;
             }
 
             return session;
@@ -28,6 +34,13 @@ export const {
             // Add access_token to the token right after signin
             // token.customField = "You can customField";
             // console.log({ token });
+            if(!token.sub) return token; //For logout
+
+            const existingUser = await getUserById(token.sub);
+
+            if(!existingUser) return token;
+
+            token.role = existingUser.role;
 
             return token;
         },
